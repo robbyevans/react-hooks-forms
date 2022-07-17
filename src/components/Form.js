292a0cc4b98b7e70d8938d6b28f7b4f61fe390f3
Evans Rutto <robbyevans001@gmail.com>
2022-07-17 15:27:28 +0300

    commit tree 109a443a74af1f0aaa1036f8d9af2905d1f91076

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,13 +4,24 @@ function Form() {
   const [firstName, setFirstName] = useState("John");
   const [lastName, setLastName] = useState("Henry");
 
+  function handleFirstNameChange(event) {
+    setFirstName(event.target.value);
+    console.log(firstName)
+  }
+
+  function handleLastNameChange(event) {
+    setLastName(event.target.value);
+    console.log(lastName)
+  }
+
   return (
     <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
+      <input type="text" onChange={handleFirstNameChange} value={firstName} />
+      <input type="text" onChange={handleLastNameChange} value={lastName} />
       <button type="submit">Submit</button>
     </form>
   );
 }
 
 export default Form;
+
